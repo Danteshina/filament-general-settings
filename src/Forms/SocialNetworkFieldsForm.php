adbc6e2b91f93c5eed49dc298938cb9a5dc669e2
Dante Shina <133533727+Danteshina@users.noreply.github.com>
2025-10-04 20:43:43 +0300

    Update SocialNetworkFieldsForm.php

--- src/Forms/SocialNetworkFieldsForm.php
+++ src/Forms/SocialNetworkFieldsForm.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Joaopaulolndev\FilamentGeneralSettings\Forms;
+namespace Danteshina\FilamentGeneralSettings\Forms;
 
 use Filament\Forms\Components\TextInput;
 use Joaopaulolndev\FilamentGeneralSettings\Enums\SocialNetworkEnum;
