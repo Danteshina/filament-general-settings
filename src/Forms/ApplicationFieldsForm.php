289bf19653c8ae47cf25da74f2d2fdc9d13b628a
Dante Shina <133533727+Danteshina@users.noreply.github.com>
2025-10-04 20:42:30 +0300

    Update ApplicationFieldsForm.php

--- src/Forms/ApplicationFieldsForm.php
+++ src/Forms/ApplicationFieldsForm.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Joaopaulolndev\FilamentGeneralSettings\Forms;
+namespace Danteshina\FilamentGeneralSettings\Forms;
 
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\FileUpload;
