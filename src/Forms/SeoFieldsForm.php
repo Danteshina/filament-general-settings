dbc57a190ba41726cc1d1227791369c65cf8dbc4
Dante Shina <133533727+Danteshina@users.noreply.github.com>
2025-10-04 20:43:49 +0300

    Update SeoFieldsForm.php

--- src/Forms/SeoFieldsForm.php
+++ src/Forms/SeoFieldsForm.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Joaopaulolndev\FilamentGeneralSettings\Forms;
+namespace Danteshina\FilamentGeneralSettings\Forms;
 
 use Filament\Forms\Components\KeyValue;
 use Filament\Forms\Components\TextInput;
